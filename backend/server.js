829c6e0f52d1c8d45691881288362fccafa2dccf
Karthikayini28 <karthisr.wiselearnz@gmail.com>
2024-09-16 17:08:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:[""],
+    methods:["GET","POST"],
+    credentials:true
+  }
+));
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/TodoList", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://karthisrwiselearnz:Kkam@2803@cluster0.vak6v.mongodb.net/Todo/retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(`MongoDB connection error: ${err.message}`));
 
