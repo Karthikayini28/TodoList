fba2e9983c68a00c6ff9425e956e7d9afe5c51f1
Karthikayini28 <karthisr.wiselearnz@gmail.com>
2024-09-16 18:33:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(cors());
+app.use(cors(
+  origin:["https://vercel.com/karthikayinis-projects/todo-list-frontend"],
+  methods:["POST","GET","PATCH","DELETE"],
+  Credentials:true
+  
+));
 
 
 app.use(express.json());
