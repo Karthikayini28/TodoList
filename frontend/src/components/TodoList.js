b5e5f61e729b1cf061c97dddcbcbd7b7c0df6123
Karthikayini28 <karthisr.wiselearnz@gmail.com>
2024-09-16 18:34:18 +0530

    Update TodoList.js

--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const api = 'https://todo-list-demoapi.vercel.app/todos';
+const api = 'https://todo-listapi.vercel.app/todos';
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
