41b47dc0e862a6763e50672af32501efdf09d0fe
Karthikayini28 <karthisr.wiselearnz@gmail.com>
2024-09-16 18:19:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,13 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // or specify the exact origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 
 mongoose.connect("mongodb+srv://karthisrwiselearnz:Kkam@2803@cluster0.vak6v.mongodb.net/Todo?retryWrites=true&w=majority&appName=Cluster0")
