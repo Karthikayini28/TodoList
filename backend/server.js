86ee7cf3f22775bf1c1532c2eadb9a7f206f42e1
Karthikayini28 <karthisr.wiselearnz@gmail.com>
2024-09-16 18:21:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // or specify the exact origin
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+
 
 app.use(express.json());
 
